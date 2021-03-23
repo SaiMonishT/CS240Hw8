033687642c45268a4b2320f479dbae833c1d3d84
SaiMonishT <70455586+SaiMonishT@users.noreply.github.com>
2021-03-23 17:15:27 -0400

    took some parts out

--- deallocate_list.c
+++ deallocate_list.c
@@ -10,13 +10,13 @@ int deallocate_list(Student_Record *linked_list) {
     if(linked_list == NULL) {
         return -1;
     } else {
-        struct Student* tmp = malloc(sizeof(struct Student));
+        struct Student* tmp;
 
     while (linked_list != NULL)
     {
-       tmp = linked_list;
+       //tmp = linked_list;
        linked_list = linked_list->next;
-       free(tmp);
+      // free(tmp);
     } //while 
      /*   for(struct Student *temp = linked_list; temp != NULL; temp = temp -> next) {
            
